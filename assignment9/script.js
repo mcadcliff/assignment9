f5a7f907e09822b4ddf309a5dc62f30a86f3aa26
mcadcliff <44850474+mcadcliff@users.noreply.github.com>
2019-03-31 20:52:28 -0500

    initial commit

---
+++ assignment9/script.js
@@ -0,0 +1,20 @@
+var llamaFacts = new Vue({
+    el: '#llamaFacts',
+    data: {
+    fact1: "1. According to the Pittsburgh Zoo, when a llama has an issue with another llama it will stick out its tongue to express displeasure.",
+    fact2: "2. BREAKING: Two llamas were on the run today in Arizona 2.26.'15",
+    fact3: "3. According to legend, the Spaniards, who had never seen llamas before, kept asking what they were called (“¿Cómo se llama?”)—and so the Incans thought “llama” was the Spanish name for the animals.",
+    fact4: "4. Llamas are pack animals, and can carry 50 to 75 pounds up to 20 miles. If you overload the animals, though, they’ll refuse to move. According to National Geographic.",
+    fact5: "5. Llamas communicate with each other through a variety of ear, tail and body postures.",
+    fact6: "6. Scientific name: Lama glama.",
+    fact7: "7. Llamas typically live for 15 to 25 years, with some individuals surviving 30 years or more.",
+    fact8: "8. They have 3 stomach compartments...",
+    fact9: "9. Living in rarefied air, at altitudes of up to 13,000 feet above sea level, their specialized blood has oval shaped corpuscles and high amounts of red blood cells which allow for added oxygen absorption.",
+    fact10: true,
+    fact10: "10. A single llama can produce about 4 pounds of wool."
+    }
+});
+
+
+
+
